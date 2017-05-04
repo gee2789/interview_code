22657e9ac5f08c0f75319220d5c88ed83c348feb
gee2789 <geeyung.li@gmail.com>
2017-05-04 11:08:05 -0400

    multiply everything except index

--- 2_array_product.js
+++ 2_array_product.js
@@ -20,22 +20,86 @@
 // 2a. Take the first number in array (first iteration)
 //    2b. Multiply across all other elements in the array (second iteration)
 //      2bc. iterate across all indexes EXCEPT the current one and multiply everything.
-//            {if index == count}
+//            {if index !== count}
 //      2c. take the result and push it into a new array.
 // 3a.  move to the next number in the array
 //    3b.
-const array = [1, 7, 3, 5]
+const array = [1, 7, 0, 3]
 const new_array = []
 
 //brute force method below
   array.map((number, index)=>{ // 0n
     array_length = array.length //set the stuff
+    current_product = 1
       for (var i = 0; i < array_length; i++){ //0n^2
         if (i !== index){
-
+          current_product = array[i] * current_product
         }
+      }
+      new_array.push(current_product)
+    })
 
+    //---It works!!!--//
+
+//greedy method below.
+
+//Think about how we can be STORING patterns.
+//In this example think about the different patterns of multiplicatio
+// [7*(0*3), 1*(0*3), 1*7*3]
+
+//step1. Create two arrays.
+//    1a. One for all values BEFORE indices
+//    1b. One for all values AFTER indicies
+//      1c.Run two for loops.  This is 0(2n) which is the same as 0(n)
+
+var product_before_index = []
+// for each integer, find the product of all the integers
+
+var productSoFar = 1; // before it, storing the total product so far each time
+for (var i = 0; i < array.length; i++) {
+    productsOfAllIntsBeforeIndex[i] = productSoFar;
+    productSoFar *= array[i];
+}
+
+var product_after_index = [];
+
+var productSoFar = 1;
+for (var i = intArray.length - 1; i >= 0; i--) {
+    productsOfAllIntsAfterIndex[i] = productSoFar;
+    productSoFar *= intArray[i];
+}
+
+// this greedy method requires too many arrays.  You'd have to then multiply both of these according to their indexes.
+
+
+//------FINAL GREEDY SOLUTION------//
+
+function getProductsOfAllIntsExceptAtIndex(intArray) {
+
+    // make sure we have at least 2 prices
+    if (intArray.length < 2) {
+        throw new Error('Getting the product of numbers at other indices requires at least 2 numbers');
     }
 
-  })
+    var productsOfAllIntsExceptAtIndex = [];
+
+    // for each integer, we find the product of all the integers
+    // before it, storing the total product so far each time
+    var productSoFar = 1;
+    for (var i = 0; i < intArray.length; i++) {
+        productsOfAllIntsExceptAtIndex[i] = productSoFar;
+        productSoFar *= intArray[i];
+    }
+
+    // for each integer, we find the product of all the integers
+    // after it. since each index in products already has the
+    // product of all the integers before it, now we're storing
+    // the total product of all other integers
+    productSoFar = 1;
+    for (var j = intArray.length - 1; j >= 0; j--) {
+        productsOfAllIntsExceptAtIndex[j] *= productSoFar;
+        productSoFar *= intArray[j];
+    }
+
+    return productsOfAllIntsExceptAtIndex;
 }
